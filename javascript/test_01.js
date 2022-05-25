5160f56228134dfa850e77357ef3cf671a320ca7
Sourav Roy <sroy3166@gmail.com>
2022-05-25 22:08:39 +0530

    test question

---
+++ javascript/test_01.js
@@ -0,0 +1,71 @@
+
+// **********************   Donot execute the code.   ***********************
+/*
+Q1) Write the Output
+var a = 10;
+
+console.log(a);
+
+function task(){
+     var a = 10;
+     console.log(a);
+     
+     if(true){
+          var a = 30;
+          console.log(a);
+     }
+
+     console.log(a);
+}
+
+task();
+
+console.log(a);
+*/
+
+/* 
+Q2) Guess the output.
+
+var fn = function (a){
+     var a = 10;
+     console.log(a);
+}
+
+fn();
+*/
+
+
+/* 
+Q3) Guess the output.
+
+fn();
+var fn = function (a){
+     console.log(b);
+     var b = a + 2;
+}
+*/
+
+
+/*
+Q4) Guess the output.
+
+let a = 10;
+const arr1 = [1, 3, 10, 23, 4];
+let arr2 = [5, 9, 1, 22, 7];
+arr1[2] = a;
+arr2[1] = a;
+
+console.log(arr1);
+console.log(arr2);
+*/
+
+/*
+Q5) You are provided a string and an array of products. You have to find out that any value of that array contains the string or not.
+Example: 
+Input: str: "lap", products: ["computer", "mobile", "guiter", "book", "laptop", "flower"]
+Output: Yes present
+
+Input: str: "memory", products: ["computer", "mobile", "guiter", "book", "laptop", "flower"]
+Output: Sorry no such product found!
+
+*/
