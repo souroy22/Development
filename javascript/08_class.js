9d4486ed3d51e131a67827dc0f1984658a0e3103
Sourav Roy <sroy3166@gmail.com>
2022-06-02 01:44:05 +0530

    added clouser defination

--- javascript/08_class.js
+++ javascript/08_class.js
@@ -1,4 +1,26 @@
 // ================================= Clouser ======================================
+/*
+
+What is Clouser?
+ans: Function along with its lexical scope bundle together, forms a clouser.
+
+Now Interviewr may ask to explain what you have just said.
+ans: Give this below example and explain.
+
+function outer (b) {
+     function inner(re){
+          b = b + 1;
+          console.log(a);
+     }
+     return inner;
+}
+
+const x = outer();
+x();
+
+*/
+
+
 let gbl = 11;
 let a = 10;
 function outer (b) {
