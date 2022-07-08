e2e6ba3b6a692bf50439d0c22b88eda958679486
souroy22 <sroy3166@gmail.com>
2022-07-08 19:58:26 +0530

    added price of the products in the card

--- React/03_class/shopping_cart/src/components/Products/views/Product.js
+++ React/03_class/shopping_cart/src/components/Products/views/Product.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { addToCart, decreaseCounter, increaseCounter } from "../../Cart/action-creator";
+import {
+  addToCart,
+  decreaseCounter,
+  increaseCounter,
+} from "../../Cart/action-creator";
 import { useDispatch, useSelector } from "react-redux";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,7 +12,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faIndianRupeeSign } from "@fortawesome/free-solid-svg-icons";
 import { addToWishList, removeFromWishList } from "../action-creator";
 import { useLocation } from "react-router-dom";
 
@@ -31,12 +35,12 @@ const Product = ({ product, saveInCart, isCartPage }) => {
   const dispatch = useDispatch();
 
   const isAddedInCart = (id) => {
-    const indx = cartProducts.findIndex(prod => prod.id === id);
+    const indx = cartProducts.findIndex((prod) => prod.id === id);
     return indx >= 0;
-  }
+  };
 
   return (
-    <Card sx={{ width: 365, margin: "10px", height: "370px" }}>
+    <Card sx={{ width: 365, margin: "10px", height: "400px" }}>
       <CardMedia
         component="img"
         alt="green iguana"
@@ -44,7 +48,7 @@ const Product = ({ product, saveInCart, isCartPage }) => {
         image={product.image}
         style={{ objectFit: "scale-down" }}
       />
-      <CardContent style={{ height: "160px" }}>
+      <CardContent style={{ height: "190px" }}>
         <Typography gutterBottom variant="h5" component="div">
           {product.title.length > 30
             ? product.title.slice(0, 30) + " ..."
@@ -54,6 +58,14 @@ const Product = ({ product, saveInCart, isCartPage }) => {
           {product.description.length > 150
             ? product.description.slice(0, 100) + " ..."
             : product.description}
+        </Typography>
+        <Typography variant="h5" color="text.secondary" style={{ marginTop: "5px" }}>
+          Price:
+          <FontAwesomeIcon
+            style={{ marginRight: "5px" }}
+            icon={faIndianRupeeSign}
+          />
+          {Math.round(product.price)}
         </Typography>
       </CardContent>
       {!isCartPage && (
@@ -68,18 +80,24 @@ const Product = ({ product, saveInCart, isCartPage }) => {
             onClick={addOrRemove}
             variant="contained"
             size="large"
-            style={{ backgroundColor: product.isWishListed ? "#5A20CB" : "#DE4839" }}
+            style={{
+              backgroundColor: product.isWishListed ? "#5A20CB" : "#DE4839",
+            }}
           >
             <FontAwesomeIcon style={{ marginRight: "5px" }} icon={faHeart} />
             {product.isWishListed ? "wishlisted" : "wishlist"}
           </Button>
           <Button
             variant="contained"
-            style={{backgroundColor: isAddedInCart(product.id) ? '#758283' : '#3DBE29'}}
+            style={{
+              backgroundColor: isAddedInCart(product.id)
+                ? "#758283"
+                : "#3DBE29",
+            }}
             size="large"
             onClick={() => dispatch(addToCart(product))}
           >
-            {isAddedInCart(product.id) ? 'Added to cart' : 'Add to cart'}
+            {isAddedInCart(product.id) ? "Added to cart" : "Add to cart"}
           </Button>
         </CardActions>
       )}
@@ -102,7 +120,11 @@ const Product = ({ product, saveInCart, isCartPage }) => {
             variant="contained"
             color="success"
             size="large"
-            style={{ backgroundColor: "#E21717", fontSize: '24px', fontWeight: 'bolder' }}
+            style={{
+              backgroundColor: "#E21717",
+              fontSize: "24px",
+              fontWeight: "bolder",
+            }}
             onClick={() => dispatch(decreaseCounter(product.id))}
           >
             -
@@ -112,7 +134,11 @@ const Product = ({ product, saveInCart, isCartPage }) => {
             variant="contained"
             color="success"
             size="large"
-            style={{ backgroundColor: "#66AD47", fontSize: '24px', fontWeight: 'bolder' }}
+            style={{
+              backgroundColor: "#66AD47",
+              fontSize: "24px",
+              fontWeight: "bolder",
+            }}
             onClick={() => dispatch(increaseCounter(product.id))}
           >
             +
